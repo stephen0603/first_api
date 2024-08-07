d975e6a0a49c9ad5280d20c3ddc09c8f4a1cf432
Stephen <stephen850603@gmail.com>
2024-08-07 16:34:16 +0800

    update all modals

--- app.py
+++ app.py
@@ -1,26 +1,38 @@
+import os
+
 from flask import Flask
 from flask_smorest import Api
+
+from db import db
+import models
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 
-app = Flask(__name__)
 
-# link exceptions
-app.config["PROPAGATE_EXCEPTIONS"] = True   
-app.config["API_TITLE"] = "Stores REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-# where the root api is
+def create_app(db_url=None):
+    app = Flask(__name__)
 
-app.config["OPENAPI_URL_PREFIX"] = "/"  
+    # link exceptions
+    app.config["PROPAGATE_EXCEPTIONS"] = True   
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    # where the root api is
 
-# ---- tell flask-smorest to use swagger
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui" 
-# api
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-# ==> where to load the code
-api = Api(app)
+    app.config["OPENAPI_URL_PREFIX"] = "/"  
+    # ---- tell flask-smorest to use swagger
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui" 
+    # api
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    # ==> where to load the code
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL","sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    
+    api = Api(app)
 
-api.register_blueprint(ItemBlueprint)
-api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+
+    return app
--- resources/store.py
+++ resources/store.py
@@ -1,56 +1,44 @@
-import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint,abort
-from db import stores
+from db import db
 from schemas import StoreSchema
+from models import StoreModel
+from sqlalchemy.exc import SQLAlchemyError,IntegrityError
 
 blp = Blueprint("store",__name__,description="Operations on stores.")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self,store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404,message="Store not found")
+        store = StoreModel.get_or_404(store_id)
+        return store
 
     def delete(self,store_id):
-        try:
-            del stores[store_id]
-            return {"message":"store deleted."}
-        except KeyError:
-            abort(404, message="store not found!")
-
-    def put(self,store_id):
-        store_data = request.get_json()
-        if "name" not in store_data:
-            abort(400, message="Bad request. Ensure 'name' are included in the JSON payload.")
-        
-        try:
-            store = stores[store_id]
-            store |= store_data
-
-            return store
-        except KeyError:
-            abort(404, message="Item not found.")
-
+        store = StoreModel.query.get_or_404(store_id)
+        raise NotImplementedError("Deleting a store is not implemented.")
 
 @blp.route("/store")
 class StoreList(MethodView):
     @blp.response(200,StoreSchema(many=True))
     def get(self):
-        return stores.values()
+        return StoreModel.query.all()
     
     @blp.arguments(StoreSchema)
     @blp.response(200,StoreSchema)
     def post(self,store_data):
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(400,message=f"Store already exists.")
+        store = StoreModel(**store_data)
 
-        store_id = uuid.uuid4().hex
-        store = {**store_data,"id":store_id}
-        stores[store_id] = store
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(
+                400,
+                message="A store with that name already exists."
+            )
+        except SQLAlchemyError:
+            abort(500, message="An error occurred creating the store.")
 
-        return store
+        return store 
